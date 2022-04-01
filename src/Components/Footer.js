43bd11d53de54cba422073d46ca79c47e7e79629
Meet Savani <savanimeet98@gmail.com>
2022-04-01 17:02:15 -0500

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -25,7 +25,7 @@ class Footer extends Component {
               <ul className="copyright">
                 <li>&copy; Copyright 2021</li>
                 <li>
-                  Design by Open Source, Edited by $nbsp;
+                  Design by Open Source, Edited by 
                   <a title="Styleshout" href="#home">
                     Meet Savani
                   </a>
